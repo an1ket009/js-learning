2af01467ea4951a35ede272858210d5ccaffed47
an1ket009 <138725929+an1ket009@users.noreply.github.com>
2024-02-07 14:05:32 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 5466
+let accountEmail = "aniket@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+console.log(accountId);
+accountPassword ="87643"
+accountCity = "Bangalore"
+console.table([accountEmail, accountId, accountPassword, accountCity])
+/* prefer not to use var becoz of issues in scope
+  scope of var is entire function
+  scope of let is block*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) // we are using nodejs not browser so it will give error
+let namee= "aniket"
+let age=18
+let isLoggedIn = true
+console.log(namee)
+//number => 2 to power 53
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+console.log(typeof 'anni');
+
